7296cea4e16921f1476e9b7852774cd7d61c1ef9
Efield44444235364 <efi3lx@gmail.com>
2025-05-08 17:06:44 +0700

    Update Placeid.lua

--- Placeid.lua
+++ Placeid.lua
@@ -11,33 +11,6 @@ end
 local fps = "120"
 local SetFps = loadstring(game:HttpGet("https://raw.githubusercontent.com/Efield44444235364/Ff/refs/heads/main/Fpaload2(none).lua"))()
 
---fps boost
-_G.Settings = {
-    Players = {
-        ["Ignore Me"] = true, -- Ignore your Character
-        ["Ignore Others"] = true -- Ignore other Characters
-    },
-    Meshes = {
-        Destroy = false, -- Destroy Meshes
-        LowDetail = false -- Low detail meshes (NOT SURE IT DOES ANYTHING)
-    },
-    Images = {
-        Invisible = false, -- Invisible Images
-        LowDetail = false, -- Low detail images (NOT SURE IT DOES ANYTHING)
-        Destroy = false, -- Destroy Images
-    },
-    ["No Particles"] = false, -- Disables all ParticleEmitter, Trail, Smoke, Fire and Sparkles
-    ["No Camera Effects"] = true, -- Disables all PostEffect's (Camera/Lighting Effects)
-    ["No Explosions"] = true, -- Makes Explosion's invisible
-    ["No Clothes"] = true, -- Removes Clothing from the game
-    ["Low Water Graphics"] = true, -- Removes Water Quality
-    ["No Shadows"] = true, -- Remove Shadows
-    ["Low Rendering"] = true, -- Lower Rendering
-    ["Low Quality Parts"] = false -- Lower quality parts
-}
-loadstring(game:HttpGet("https://raw.githubusercontent.com/Dragonkung/Dragonkung/main/FPSBooster.lua"))()
-
-
 local executor = identifyexecutor and identifyexecutor() or "Unknown Executor"
 
 pcall(function()
@@ -91,7 +64,9 @@ elseif allowedPlaceIds[GameP] then
     print("script has been executed")
     sendNotification("Script ID [" .. GameP .. "]", "Loading Script.")
     
-    -- โหลดสคริปต์หลัก
+    --fps boost
+    loadstring(game:HttpGet("https://pastebin.com/raw/pk4gFFBz"))()
+    --main script 
     loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/705e7fe7aa288f0fe86900cedb1119b1.lua"))()
 
 else
