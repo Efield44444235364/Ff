5564e103dfcd91f611ba66563f4958688a0f6575
Efield44444235364 <efi3lx@gmail.com>
2025-06-22 09:13:02 +0700

    Update breakin load.lua
    [ + ] Add Place Id (Main Game)

--- breakin load.lua
+++ breakin load.lua
@@ -2,7 +2,7 @@ local url
 
 if game.PlaceId == 3851622790 or game.PlaceId == 4620170611 then
     url = "https://raw.githubusercontent.com/Efield44444235364/Ff/refs/heads/main/Breakin1%20Command.lua"
-elseif game.PlaceId == 13864661000 then
+elseif game.PlaceId == 13864661000 or game.PlaceId == 13864667823 then
     url = "https://raw.githubusercontent.com/Efield44444235364/Ff/refs/heads/main/Command%20Breakin%202.lua"
 end
 
