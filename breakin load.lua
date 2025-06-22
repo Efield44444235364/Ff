7d3a59ad8a23c5dfa9e04d1f880cf8d8c033da51
Efield44444235364 <efi3lx@gmail.com>
2025-06-22 14:10:33 +0700

    Update breakin load.lua

--- breakin load.lua
+++ breakin load.lua
@@ -4,7 +4,7 @@ if game.PlaceId == 3851622790 or game.PlaceId == 4620170611 then
     url = "https://raw.githubusercontent.com/Efield44444235364/Ff/refs/heads/main/Breakin1%20Command.lua"
 elseif game.PlaceId == 13864661000 or game.PlaceId == 13864667823 then
     url = "https://raw.githubusercontent.com/Efield44444235364/Ff/refs/heads/main/Command%20Breakin%202.lua" 
-elseif game.PlaceId == 4954096313 the
+elseif game.PlaceId == 4954096313 or game.PlaceId == 5096191125 then
     url = "https://raw.githubusercontent.com/Efield44444235364/Ff/refs/heads/main/FieldZ.lua"
 end
 
